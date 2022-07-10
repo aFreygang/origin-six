a3b61674689eb08a816770438fad3108bf22020b
André Luiz Freygang <72108085+aFreygang@users.noreply.github.com>
2022-07-10 14:24:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// DOM Document Object Model
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const menu_itens = document.querySelectorAll('.grid li')
+
+for (const botao of menu_itens) {
+  botao.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* sombra no header quando scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+})
